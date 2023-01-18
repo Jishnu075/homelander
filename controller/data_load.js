5f4fe2c36f684587f01d681d7170f7188c82aecc
Jishnu075 <jishnubiju075@gmail.com>
2023-01-18 21:09:50 +0530

    refactor

--- controller/data_load.js
+++ controller/data_load.js
@@ -1,15 +1,44 @@
+import { House } from "../model/house.js";
+
 class DataLoader {
   constructor() {
-    this.onClick = function onClick() {
+    this.onClick = function onClick(csvFile) {
       const input = csvFile.files[0];
       const fileReader = new FileReader();
       fileReader.onload = (event) => {
-        const fileData = event.target.result;
-        const parsedData = dataParse(fileData);
-        const houses = createHousesList(parsedData);
-        console.log(houses);
+        onLoad(event);
       };
       fileReader.readAsText(input);
     };
+
+    // func for onload of file
+    function onLoad(event) {
+      const fileData = event.target.result;
+      const parsedData = dataParse(fileData);
+      const houses = createHousesList(parsedData);
+      return houses;
+    }
+
+    // converts text data to object
+    function dataParse(fileAsText) {
+      const lines = fileAsText.replace(/(\r\n|\n|\r)/gm, "#").split("#");
+      lines.splice(0, 1);
+      // to filter out all the empty elements from arr
+      const newArr = lines.filter((element) => element.length > 1);
+      return newArr;
+    }
+    // to create a list of house of obj as arr
+    function createHousesList(arr) {
+      const housesArr = [];
+      arr.map((x, i) => {
+        const eachItem = x.split(",");
+        const [a, b, c, d, e, f, g] = eachItem;
+        const house = new House(a, b, c, d, e, f, g);
+        //house to array(list of houses)
+        housesArr.push(house);
+      });
+      return housesArr;
+    }
   }
 }
+export { DataLoader };
--- view/script.js
+++ view/script.js
@@ -1,39 +1,10 @@
 "use strict";
-import { House } from "../model/house.js";
+import { DataLoader } from "../controller/data_load.js";
 
-window.addEventListener("load", () => {
-  const uploadBtn = document.getElementById("upload-file-btn");
-  const csvFile = document.getElementById("file-selector");
-  uploadBtn.addEventListener("click", () => {
-    const input = csvFile.files[0];
-    const fileReader = new FileReader();
-    fileReader.onload = (event) => {
-      const fileData = event.target.result;
-      const parsedData = dataParse(fileData);
-      const houses = createHousesList(parsedData);
-      console.log(houses);
-    };
-    fileReader.readAsText(input);
-  });
+const uploadBtn = document.getElementById("upload-file-btn");
+const csvFile = document.getElementById("file-selector");
+uploadBtn.addEventListener("click", () => {
+  const dataloader = new DataLoader();
+  const houseList = dataloader.onClick(csvFile);
+  console.log(houseList, 1234);
 });
-
-// converts text data to object
-function dataParse(fileAsText) {
-  const lines = fileAsText.replace(/(\r\n|\n|\r)/gm, "#").split("#");
-  lines.splice(0, 1);
-  // to filter out all the empty elements from arr
-  const newArr = lines.filter((element) => element.length > 1);
-  return newArr;
-}
-
-function createHousesList(arr) {
-  const housesArr = [];
-  arr.map((x, i) => {
-    const eachItem = x.split(",");
-    const [a, b, c, d, e, f, g] = eachItem;
-    const house = new House(a, b, c, d, e, f, g);
-    //house to array(list of houses)
-    housesArr.push(house);
-  });
-  return housesArr;
-}
