c9920d75a263f39d9564592b7ed493cc4a03f960
Jishnu075 <jishnubiju075@gmail.com>
2023-01-19 16:08:41 +0530

    data-parser

---
+++ controller/data_parse.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// func to convert parse string to Object for using localStorage.data
+function stringToJson() {
+  return JSON.parse(localStorage.data);
+}
+const houseDataLength = stringToJson().length;
+
+export { houseDataLength, stringToJson };
