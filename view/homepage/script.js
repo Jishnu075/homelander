a9f4bab516a04850d14282d6560114f741ea36d2
Jishnu075 <jishnubiju075@gmail.com>
2023-01-19 10:07:10 +0530

    refactor

--- view/homepage/script.js
+++ view/homepage/script.js
@@ -1,10 +1,9 @@
 "use strict";
-import { DataLoader } from "../controller/data_load.js";
+import { DataLoader } from "../../controller/data_load.js";
 
 const uploadBtn = document.getElementById("upload-file-btn");
 const csvFile = document.getElementById("file-selector");
 uploadBtn.addEventListener("click", () => {
   const dataloader = new DataLoader();
   dataloader.onClick(csvFile);
-  // console.log(JSON.parse(localStorage.data));
 });
