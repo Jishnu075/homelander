72504fc14f983b9b2157158eaca522ace7ac159d
Jishnu075 <jishnubiju075@gmail.com>
2023-01-19 16:06:01 +0530

    filters, and getData

---
+++ controller/data_handler.js
@@ -0,0 +1,44 @@
+"use strict";
+
+import { stringToJson } from "./data_parse.js";
+
+// func that returns houseData as whole if index is not provided
+function getEachHouseData(index = "default") {
+  const jsonData = stringToJson();
+  return index === "default" ? jsonData : jsonData[index];
+}
+const houseData = getEachHouseData();
+function bedNumberFilter(bedNum) {
+  Object.entries(houseData).forEach((entry) => {
+    const [key, value] = entry;
+    if (bedNum === value["beds"]) {
+      return value;
+    } else {
+      return false;
+    }
+  });
+}
+function priceFilter(min, max) {
+  Object.entries(houseData).forEach((entry) => {
+    const [key, value] = entry;
+    if (value["price"] >= min && value["price"] <= max) {
+      console.log(value);
+      return value;
+    } else {
+      return false;
+    }
+  });
+}
+function sqftFilter(min, max) {
+  Object.entries(houseData).forEach((entry) => {
+    const [key, value] = entry;
+    if (value["sqft"] >= min && value["sqft"] <= max) {
+      console.log(value);
+      return value;
+    } else {
+      return false;
+    }
+  });
+}
+
+export { getEachHouseData, bedNumberFilter, priceFilter, sqftFilter };
